0487d2c93370a15450e151cd3162a224a7487947
Yuri-Rangel-Rabelo-Palheta <mavericyuri@gmail.com>
2022-01-24 07:54:26 -0300

    ind

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,12 @@
+import { Head } from "next/document";
+import Header from "../components/Header";
 
 export default function Home() {
   return (
     
 
-    <head>
+    <Header />
         
-
-    </head>
 
 
   );
