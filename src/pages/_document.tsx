e1fcc5c5489694eacedc4d5daadc05b4bba7c11a
Yuri-Rangel-Rabelo-Palheta <mavericyuri@gmail.com>
2022-01-24 07:48:24 -0300

    create next, react

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -44,7 +44,8 @@ export default class MyDocument extends Document {
           
           <link rel="preconnect" href="https://fonts.googleapis.com"/> 
           <link rel="preconnect" href="https://fonts.gstatic.com" />
-          <link href="https://fonts.googleapis.com/css2?family=JetBrains+Mono:wght@200&family=Montserrat:wght@300;400;500;600;700&display=swap" rel="stylesheet"/>
+          <link href="https://fonts.googleapis.com/css2?family=JetBrains+Mono:wght@200&family=Montserrat:wght@300;400;500;600;700&display=swap" 
+                rel="stylesheet"/>
           <meta charSet="utf-8" />
           <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         </Head>
