0cdd48ff83278deb14cdb55b55a9e94a4972347e
Yuri-Rangel-Rabelo-Palheta <mavericyuri@gmail.com>
2022-01-27 12:10:57 -0300

    imagem home hero

--- src/components/HomeHero/index.tsx
+++ src/components/HomeHero/index.tsx
@@ -1,10 +1,18 @@
 import { Container } from './styles';
-
-
+import picture from '../../assets/pessoa.webp';
+import {TextContainer} from './styles';
 
 function HomeHero() {
   return (
     <Container>
+      <img src={picture} alt="Minha foto" />
+      <div>
+        <TextContainer>
+          <h1>Olá</h1>
+          <h2 classname = "nessuno">Meu nome é Yuri  </h2>
+
+        </TextContainer>
+      </div>
       <h1>HomeHero</h1>
       
     </Container>
--- src/components/HomeHero/styles.ts
+++ src/components/HomeHero/styles.ts
@@ -1,5 +1,85 @@
 import styled from 'styled-components';
 
-export const Container = styled.div`
+export const Container = styled.section`
+    width: 100%;
+    display: flex;
+    gap: 2rem;
+    align-items: center;
+    justify-content: center;
+    margin-top: 5rem;
+
+    > img {
+        width: 40rem;
+        flex: 1;
+
+
+    }
+
+    > div{
+        flex: 4;
+
+
+    }
+
+    @media(max-width: 1450px){
+        > img {
+            width: 30rem;
+        }
+
+        > div {
+            flex: 1;
+        }
+    }
+
+    @media(max-width: 1000px){
+        > img {
+            width: 22rem;
+        }
+
+    }
+
+    @media(max-width: 700px){
+        flex-direction: column-reverse;
+        > div {
+            width: 100%;
+        }
+
+    }
+
+
+    `;
+
+export const TextContainer = styled.section`
+    margin-botton: 2rem;
+    width: 100%;
+
+    h1{
+        font-size 8rem;
+        color: ${({theme}) => theme.primary}
+    }
+
+    h2{
+        font-size 3rem;
+        color: ${({theme}) => theme.secondary}
+        font-weigth: 400;
+    }
+
+    @media(max-width: 1450px){
+        h1{
+            font-size: 5rem;
+        }
+        h2{
+            font-size: 2rem;
+        }
+    }
+
+    @media(max-width: 1000px){
+        h1{
+            font-size: 3rem;
+        }
+        h2{
+            font-size: 1.5rem;
+        }
+    }
 
 `;
