a95ee913214cffc2c54cd1f15c1fec99a69b24df
Yuri-Rangel-Rabelo-Palheta <mavericyuri@gmail.com>
2022-01-27 09:49:16 -0300

    homehero

---
+++ src/components/HomeHero/index.tsx
@@ -0,0 +1,14 @@
+import { Container } from './styles';
+
+
+
+function HomeHero() {
+  return (
+    <Container>
+      <h1>HomeHero</h1>
+      
+    </Container>
+  );
+};
+
+export default HomeHero;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,23 @@
 import { Head } from "next/document";
 import Header from "../components/Header";
+import HomeHero from "../components/HomeHero";
+import { HomeContainer } from "../styles/HomeStyles";
+
 
 export default function Home() {
   return (
-    
+    <homecontainer>
 
-    <Header />
+      <Header />
+
+      <main classname= "container">
+        <HomeHero />
+
+      </main>
+
+    </homecontainer>
+
+    
         
 
 
